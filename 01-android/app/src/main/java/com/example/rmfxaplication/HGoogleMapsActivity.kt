e2404a797c3755a285f4288f0e74300c169a036c
Francisco_Rodríguez <francisco.rodriguez@epn.edu.ec>
2023-02-01 12:53:51 -0500

    Clase 01-02-23

--- 01-android/app/src/main/java/com/example/rmfxaplication/HGoogleMapsActivity.kt
+++ 01-android/app/src/main/java/com/example/rmfxaplication/HGoogleMapsActivity.kt
@@ -1,11 +1,91 @@
 package com.example.rmfxaplication
 
+import android.content.pm.PackageManager
+import android.icu.text.CaseMap.Title
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PackageManagerCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class HGoogleMapsActivity : AppCompatActivity() {
+    var permisos = false
+    private lateinit var mapa: GoogleMap
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hgoogle_maps2)
+        solicitarPermiso()
+        iniciarLogicaMapa()
+        establecerConfiguracionMapa()
+    }
+
+    fun iniciarLogicaMapa(){
+        val fragmentoMapa = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        fragmentoMapa.getMapAsync{ googleMap ->
+            if (googleMap != null) {
+                mapa = googleMap
+                establecerConfiguracionMapa()
+            }
+        }
+    }
+
+    fun establecerConfiguracionMapa(){
+        val contexto = this.applicationContext
+        with(mapa) {
+            val permisosFineLocation = ContextCompat
+                .checkSelfPermission(
+                    contexto,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
+            if (tienePermisos){
+                mapa.isMyLocationEnabled = true // Tenemos permisos
+                uiSettings.isMyLocationButtonEnabled = true
+            }
+            uiSettings.isMyLocationButtonEnabled = true // No tenemos aun permisos
+        }
+    }
+
+    fun anadirMarcador(latLng: LatLng, title: String){
+        mapa.addMarker(
+            MarkerOptions()
+                .position(latLng)
+                .title(title)
+        )
+    }
+
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 10f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng,zoom)
+        )
+    }
+
+    
+    fun solicitarPermiso(){
+        val contexto = this.applicationContext
+        val permisosFineLocation = ContextCompat
+            .checkSelfPermission(
+                contexto,
+                android.Manifest.permission.ACCESS_FINE_LOCATION // permiso que van a checkear
+            )
+        val tienePermisos = permisosFineLocation == PackageManager.PERMISSION_GRANTED
+        if (tienePermisos){
+            permisos = true
+        }else{
+            ActivityCompat.requestPermissions(
+                this, // Contexto
+                arrayOf( // Arreglo Permisos
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                1 // Codigo de peticion de los permisos
+            )
+        }
     }
 }
